0eac1c01f1dcca45f50adc125e1ab0accc8236e3
jorge <jorge.a.galan@gmail.com>
2021-11-02 18:37:05 -0500

    cambio en carpeta salvar

--- cambio_tamano.py
+++ cambio_tamano.py
@@ -2,12 +2,34 @@ from PIL import Image
 import os, sys
 import glob
 
-lis_imgjpg = glob.glob(r'C:\Users\jorge\Downloads\Photos/*.jpg')
+path = r"C:\Users\jorge\Downloads\Photos (1)"
+path = os.path.abspath(path)
+dirs = os.listdir( path )
+
+# Directory
+directory = "convertidas"
+
+# Parent Directory path
+parent_dir = path
+
+# Path
+path1 = os.path.join(parent_dir, directory)
+
+os.mkdir(path1)
+print("Directory '% s' created" % directory)
+
+lis_imgjpg = glob.glob(parent_dir + '/*.jpg')
+
 for item in lis_imgjpg:
-    print(item)
+    # print(item)
     basewidth = 600
     img = Image.open(item)
     wpercent = (basewidth / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    img.save(item +' resized_image.jpg')
+    # extraer nombre del archivo
+    nomb = os.path.basename(item)
+
+    print(path1 + "/" + nomb)
+    # guardar en la carpeta de convertidas
+    img.save(path1 + "/" + nomb)
